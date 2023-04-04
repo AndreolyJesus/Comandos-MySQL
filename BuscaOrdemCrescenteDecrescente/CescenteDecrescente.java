ca350cbc9f56624ba73a7b3e2da4bf6187a714bb
Andreoly Jesus <andreolyjesus@hotmail.com>
2023-04-04 11:46:37 -0300

    Comandos para busca em ordem crescente e decrescente da tabela.

---
+++ BuscaOrdemCrescenteDecrescente/CescenteDecrescente.java
@@ -0,0 +1,17 @@
+package BuscaOrdemCrescenteDecrescente;
+
+/*
+Por exemplo, para selecionar todos os dados da tabela "exemplo"
+em ordem crescente de uma coluna chamada "coluna1",
+você pode usar o seguinte comando:
+ */
+    //  SELECT * FROM exemplo ORDER BY coluna1 ASC;
+
+//------------------------------------------------------------------
+
+/*
+O "ASC" no final indica que você deseja classificar os resultados em ordem crescente.
+Se você quiser classificar os resultados em ordem decrescente,
+use "DESC" em vez de "ASC". Por exemplo:
+ */
+    //  SELECT * FROM exemplo ORDER BY coluna1 DESC;
