cb8ad34500d82c7ec39c9e83214a37edacd765f5
Andreoly Jesus <andreolyjesus@hotmail.com>
2023-04-04 11:50:51 -0300

    Comandos para selecionar todos os dados na tabela.

---
+++ SelecionarTodos/SelcecionarTodos.java
@@ -0,0 +1,11 @@
+package SelecionarTodos;
+
+
+        //  SELECT * FROM nome_da_tabela;
+/*
+O asterisco (*) indica que você deseja selecionar todas as colunas da tabela.
+Se você quiser selecionar apenas colunas específicas,
+basta listar seus nomes separados por vírgulas em vez do asterisco.
+Por exemplo:
+ */
+    //  SELECT coluna1, coluna2, coluna3 FROM nome_da_tabela;
