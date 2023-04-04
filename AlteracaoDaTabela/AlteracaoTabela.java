a05a61626cecf1427fa5ebaae3bf1ea71fcbdd44
Andreoly Jesus <andreolyjesus@hotmail.com>
2023-04-04 11:43:36 -0300

    Comandos para a alteração da tabela.

---
+++ AlteracaoDaTabela/AlteracaoTabela.java
@@ -0,0 +1,16 @@
+package AlteracaoDaTabela;
+
+/*
+ALTER TABLE nome_da_tabela
+ADD nome_da_coluna tipo_da_coluna opcoes;
+
+ALTER TABLE nome_da_tabela
+DROP COLUMN nome_da_coluna;
+
+ALTER TABLE nome_da_tabela
+MODIFY COLUMN nome_da_coluna tipo_da_coluna opcoes;
+
+ALTER TABLE nome_da_tabela
+CHANGE COLUMN nome_da_coluna novo_nome_da_coluna tipo_da_coluna opcoes;
+
+ */
